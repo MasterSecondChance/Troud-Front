14d1fb7a65838542a949efeeb037da9c31b0921e
Felipe Merchán <felipe-sc@hotmail.es>
2020-08-22 07:24:17 -0500

    They were created as the message component as the message list with responsive design.

---
+++ src/components/ListOfMessages/ListOfMessages.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Message from '../Message/Message';
+import './ListOfMessages.scss';
+
+const ListOfMessages = () => {
+  return (
+    <section className="ListOfMessages">
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+    </section>
+  )
+}
+
+export default ListOfMessages
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -5,6 +5,7 @@ import { Link, BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from '../pages/Home';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+import Inbox from '../pages/Inbox';
 import User from '../components/User';
 import FirstStep from '../components/Sign/FirstStep';
 import FullCard from '../components/FullCard/FullCard';
@@ -21,6 +22,7 @@ function App() {
         <Route exact path='/home' component={Home} />
         <Route exact path='/fullcard' component={FullCard} />
         <Route exact path='/user' component={User} />
+        <Route exact path='/inbox' component={Inbox} />
       </Switch>
     </BrowserRouter>
   );
