03dc7b2ad23a014a15c3fbee7d75c913f544d1f9
Alexander <jhonalexander9@gmail.com>
2020-08-22 12:07:34 -0500

    set hover styles in menu, fix file without folder and add temporal favicon

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -5,7 +5,7 @@ import { Link, BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from '../pages/Home';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
-import User from '../components/User';
+import User from '../components/User/User';
 import FirstStep from '../components/Sign/FirstStep';
 import FullCard from '../components/FullCard/FullCard';
 
