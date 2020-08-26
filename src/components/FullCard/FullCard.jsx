a853134cab78d9645136584491319ec8e959956b
Felipe Chávez <a.philippc@gmail.com>
2020-08-26 01:24:36 -0500

    Add FullCard desktop version

--- src/components/FullCard/FullCard.jsx
+++ src/components/FullCard/FullCard.jsx
@@ -16,44 +16,49 @@ const FullCard = () => {
       <Header />
       <div className='FullCard'>
         <div className='FullCard__Info'>
-          <div className='FullCard__Info__Header'>
-            <div className='FullCard__Info__Header--Title'>
-              <div>
-                <img className='FullCard__Info__Header--Pic' src={profilePic} alt='' />
-              </div>
-              <span>Nombre del Perfil</span>
-            </div>
-            <Link to='/modify'>
-              <FontAwesomeIcon className='options faEllipsisV' icon={faEllipsisV} title='Opciones' />
-            </Link>
-          </div>
-          <div className='FullCard__Info__Img'>
-            <img className='FullCard__Info__Img__Image' src={image} alt='Img FullCard' />
-            <div className='FullCard__Info__Img__Gender'><FontAwesomeIcon className='faMars' icon={faMars} title='Hombre' /></div>
-            <div className='FullCard__Info__Img__Size'>Talla: S</div>
-          </div>
-          <p className='FullCard__Info__Name'>Nombre de la prenda</p>
-          <div className='FullCard__Info__Desc'>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat accusantium</p>
-          </div>
-          <div className='FullCard__Details'>
-            <div className='FullCard__Details--item'>
-              <p className='FullCard__Details--item-title'>Marca</p>
-              <p className='FullCard__Details--item-text'>Standar</p>
-            </div>
-            <div className='FullCard__Details--item'>
-              <p className='FullCard__Details--item-title'>Estado</p>
-              <p className='FullCard__Details--item-text'>Casi Nuevo</p>
-            </div>
-            <div className='FullCard__Details--item'>
-              <p className='FullCard__Details--item-title'>Categoria</p>
-              <p className='FullCard__Details--item-text'>Accesorios</p>
-            </div>
-            <div className='FullCard__Details--item'>
-              <p className='FullCard__Details--item-title'>Ciudad</p>
-              <p className='FullCard__Details--item-text'>Bogota</p>
-            </div>
 
+          <div className='FullCard__imag-wrapper'>
+            <div className='FullCard__Info__Header'>
+              <div className='FullCard__Info__Header--Title'>
+                <div>
+                  <img className='FullCard__Info__Header--Pic' src={profilePic} alt='' />
+                </div>
+                <span>Nombre del Perfil</span>
+              </div>
+              <Link to='/modify'>
+                <FontAwesomeIcon className='options faEllipsisV' icon={faEllipsisV} title='Opciones' />
+              </Link>
+            </div>
+            <div className='FullCard__Info__Img'>
+              <img className='FullCard__Info__Img__Image' src={image} alt='Img FullCard' />
+              <div className='FullCard__Info__Img__Gender'><FontAwesomeIcon className='faMars' icon={faMars} title='Hombre' /></div>
+              <div className='FullCard__Info__Img__Size'>Talla: S</div>
+            </div>
+          </div>
+
+          <div className='FullCard__Info-wrapper'>
+            <p className='FullCard__Info__Name'>Nombre de la prenda</p>
+            <div className='FullCard__Info__Desc'>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat accusantium</p>
+            </div>
+            <div className='FullCard__Details'>
+              <div className='FullCard__Details--item'>
+                <p className='FullCard__Details--item-title'>Marca</p>
+                <p className='FullCard__Details--item-text'>Standar</p>
+              </div>
+              <div className='FullCard__Details--item'>
+                <p className='FullCard__Details--item-title'>Estado</p>
+                <p className='FullCard__Details--item-text'>Casi Nuevo</p>
+              </div>
+              <div className='FullCard__Details--item'>
+                <p className='FullCard__Details--item-title'>Categoria</p>
+                <p className='FullCard__Details--item-text'>Accesorios</p>
+              </div>
+              <div className='FullCard__Details--item'>
+                <p className='FullCard__Details--item-title'>Ciudad</p>
+                <p className='FullCard__Details--item-text'>Bogota</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
