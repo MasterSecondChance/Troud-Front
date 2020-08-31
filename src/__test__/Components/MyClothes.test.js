390a3a26ff9f68df2d9e074e0c7a2206ff49214e
Felipe Merchán <felipe-sc@hotmail.es>
2020-08-30 20:44:17 -0500

    Header and HeaderLight tests have been updated.

--- src/__test__/Components/MyClothes.test.js
+++ src/__test__/Components/MyClothes.test.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { mount, shallow } from 'enzyme';
-import renderer, { create } from 'react-test-renderer';
-import ProviderMock from '../../../__mocks__/providerMock';
-import MyClothes from '../../../components/MyClothes/MyClothes';
+import { shallow } from 'enzyme';
+import ProviderMock from '../../__mocks__/providerMock';
+import MyClothes from '../../components/MyClothes/MyClothes';
 
 describe('<MyClothes />', () => {
   test('Render del componente My Clothes', () => {
